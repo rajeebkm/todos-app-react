28b5316079ea05eb9b75b3af27cea26acfdd76bb
HP <rkm2541997@gmail.com>
2022-11-09 02:38:47 +0530

    Todos app added

---
+++ src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div className='text-center'>
+            This is an about page. 
+            <p>Rajeeb Kumar Malik's Todos application.</p>
+        </div>
+    )
+}
